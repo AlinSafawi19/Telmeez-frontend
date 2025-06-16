f94b9d22e437cc0b73955c9ee2c19240ff5ee28b
AlinSafawi19 <alinsafawi19@gmail.com>
2025-06-16 05:36:54 +0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/telmeez-frontend/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+  plugins: [react()]
 })
