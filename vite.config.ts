9e9700e0c705999da5a4d0f26e9730e1c97d590c
AlinSafawi19 <alinsafawi19@gmail.com>
2025-06-28 03:08:32 +0300

    M

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env['BASE_URL'],
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
